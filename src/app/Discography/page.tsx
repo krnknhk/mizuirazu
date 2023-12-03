7f6485a68cba18173f0ab8d71880173652382b92
krnknhk <kuronekonihiki@gmail.com>
2023-12-03 11:00:22 +0900

    Discography修正

--- src/app/Discography/page.tsx
+++ src/app/Discography/page.tsx
@@ -68,7 +68,9 @@ export default function Discography() {
             <Link href="/About">
               <Text _hover={{ color: "#e88700" }}>About</Text>
             </Link>
-            <Text _hover={{ color: "#e88700" }}>Live</Text>
+            <Link href="/Live">
+              <Text _hover={{ color: "#e88700" }}>Live</Text>
+            </Link>
             <Link href="https://www.youtube.com/@user-qi9nl3ru1l/videos">
               <Text _hover={{ color: "#e88700" }}>Movie</Text>
             </Link>
@@ -92,110 +94,114 @@ export default function Discography() {
         <Box height="1px" backgroundColor="gray.200" width="90%" mt={3} />
       </Box>
       <Center>
-        <Grid
-          templateRows="repeat(2, 1fr)"
-          templateColumns="repeat(8, 1fr)"
-          gap={30}
-          mt={70}
-          width="65%"
-          height="27vh"
-          cursor="pointer"
-        >
-          <GridItem
-            rowSpan={1}
-            colSpan={2}
-            backgroundImage="url('/uturu.png')"
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
-            className="shake-on-hover"
-            aspectRatio={1 / 1}
-            onClick={() => router.push("/Discography/uturu")}
-          ></GridItem>
+        <Box width="70%" height="70%" backgroundColor="#f4f4ed" mt={"10vh"}>
+          <Center>
+            <Grid
+              templateRows="repeat(2, 1fr)"
+              templateColumns="repeat(8, 1fr)"
+              gap={30}
+              my={10}
+              width="93%"
+              height="90%"
+              cursor="pointer"
+            >
+              <GridItem
+                rowSpan={1}
+                colSpan={2}
+                backgroundImage="url('/uturu.png')"
+                backgroundRepeat="no-repeat"
+                backgroundSize="cover"
+                boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
+                className="shake-on-hover"
+                aspectRatio={1 / 1}
+                onClick={() => router.push("/Discography/uturu")}
+              ></GridItem>
 
-          <GridItem
-            rowSpan={1}
-            colSpan={2}
-            backgroundImage="url('/20210828_28.jpg')"
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
-            className="shake-on-hover"
-            aspectRatio={1 / 1}
-            onClick={() => router.push("/Discography/bakeru")}
-          ></GridItem>
+              <GridItem
+                rowSpan={1}
+                colSpan={2}
+                backgroundImage="url('/20210828_28.jpg')"
+                backgroundRepeat="no-repeat"
+                backgroundSize="cover"
+                boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
+                className="shake-on-hover"
+                aspectRatio={1 / 1}
+                onClick={() => router.push("/Discography/bakeru")}
+              ></GridItem>
 
-          <GridItem
-            rowSpan={1}
-            colSpan={2}
-            backgroundImage="url('/su-mu_single.png')"
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
-            className="shake-on-hover"
-            aspectRatio={1 / 1}
-            onClick={() => router.push("/Discography/sumu")}
-          ></GridItem>
+              <GridItem
+                rowSpan={1}
+                colSpan={2}
+                backgroundImage="url('/su-mu_single.png')"
+                backgroundRepeat="no-repeat"
+                backgroundSize="cover"
+                boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
+                className="shake-on-hover"
+                aspectRatio={1 / 1}
+                onClick={() => router.push("/Discography/sumu")}
+              ></GridItem>
 
-          <GridItem
-            rowSpan={1}
-            colSpan={2}
-            backgroundImage="url('/hotonndokara_jacket.jpeg')"
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
-            className="shake-on-hover"
-            aspectRatio={1 / 1}
-            onClick={() => router.push("/Discography/hotondokara")}
-          ></GridItem>
-          <GridItem
-            rowSpan={1}
-            colSpan={2}
-            backgroundImage="url('/almostEmpty_single.png')"
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
-            className="shake-on-hover"
-            aspectRatio={1 / 1}
-            onClick={() => router.push("/Discography/hotondokara_single")}
-          ></GridItem>
+              <GridItem
+                rowSpan={1}
+                colSpan={2}
+                backgroundImage="url('/hotonndokara_jacket.jpeg')"
+                backgroundRepeat="no-repeat"
+                backgroundSize="cover"
+                boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
+                className="shake-on-hover"
+                aspectRatio={1 / 1}
+                onClick={() => router.push("/Discography/hotondokara")}
+              ></GridItem>
+              <GridItem
+                rowSpan={1}
+                colSpan={2}
+                backgroundImage="url('/almostEmpty_single.png')"
+                backgroundRepeat="no-repeat"
+                backgroundSize="cover"
+                boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
+                className="shake-on-hover"
+                aspectRatio={1 / 1}
+                onClick={() => router.push("/Discography/hotondokara_single")}
+              ></GridItem>
 
-          <GridItem
-            rowSpan={1}
-            colSpan={2}
-            backgroundImage="url('/5dgt_B3I-1.jpeg')"
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
-            className="shake-on-hover"
-            aspectRatio={1 / 1}
-            onClick={() => router.push("/Discography/shinshinshin")}
-          ></GridItem>
+              <GridItem
+                rowSpan={1}
+                colSpan={2}
+                backgroundImage="url('/5dgt_B3I-1.jpeg')"
+                backgroundRepeat="no-repeat"
+                backgroundSize="cover"
+                boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
+                className="shake-on-hover"
+                aspectRatio={1 / 1}
+                onClick={() => router.push("/Discography/shinshinshin")}
+              ></GridItem>
 
-          <GridItem
-            rowSpan={1}
-            colSpan={2}
-            backgroundImage="url('afternoon-night.jpeg')"
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
-            className="shake-on-hover"
-            aspectRatio={1 / 1}
-            onClick={() => router.push("/Discography/hiruyoru")}
-          ></GridItem>
+              <GridItem
+                rowSpan={1}
+                colSpan={2}
+                backgroundImage="url('afternoon-night.jpeg')"
+                backgroundRepeat="no-repeat"
+                backgroundSize="cover"
+                boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
+                className="shake-on-hover"
+                aspectRatio={1 / 1}
+                onClick={() => router.push("/Discography/hiruyoru")}
+              ></GridItem>
 
-          <GridItem
-            rowSpan={1}
-            colSpan={2}
-            backgroundImage="url('/monochrome.jpeg')"
-            backgroundRepeat="no-repeat"
-            backgroundSize="cover"
-            boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
-            className="shake-on-hover"
-            aspectRatio={1 / 1}
-            onClick={() => router.push("/Discography/monochrome")}
-          ></GridItem>
-        </Grid>
+              <GridItem
+                rowSpan={1}
+                colSpan={2}
+                backgroundImage="url('/monochrome.jpeg')"
+                backgroundRepeat="no-repeat"
+                backgroundSize="cover"
+                boxShadow="0px 0px 10px 0px rgba(0,0,0,0.5)"
+                className="shake-on-hover"
+                aspectRatio={1 / 1}
+                onClick={() => router.push("/Discography/monochrome")}
+              ></GridItem>
+            </Grid>
+          </Center>
+        </Box>
       </Center>
     </Flex>
   );
