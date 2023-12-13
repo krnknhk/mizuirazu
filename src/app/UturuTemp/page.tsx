0fc06288cd034c7d0b2305e945f7947715be57e2
krnknhk <kuronekonihiki@gmail.com>
2023-12-14 01:20:27 +0900

    uturu追加9

--- src/app/UturuTemp/page.tsx
+++ src/app/UturuTemp/page.tsx
@@ -5,6 +5,11 @@ import Footer from "@/app/Footer";
 import Menu from "@/app/menu";
 import SpMenu from "@/app/spmenu";
 import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
   AspectRatio,
   Box,
   Breadcrumb,
@@ -469,17 +474,82 @@ export default function UturuTemp() {
                       　東京を拠点とする4人組バンド、水いらずが届けてくれた新曲
                       「uturu」は、構想から約1年をかけた力作で、東京のインディ・ミュージックの分厚い歴史と未来を感じ取ることができる。
                     </Text>
-                    <Link href="/ReviewHiwatt">
-                      <Text
-                        fontSize="18px"
-                        fontWeight="bold"
-                        fontFamily="japanese2"
-                        textColor="#55499b"
-                        as="u"
-                      >
-                        Read more
-                      </Text>
-                    </Link>
+
+                    <Accordion allowToggle>
+                      <AccordionItem>
+                        <h2>
+                          <AccordionButton>
+                            <Box as="span" flex="1" textAlign="left">
+                              <Text
+                                fontSize="16px"
+                                fontWeight="bold"
+                                fontFamily="japanese2"
+                                textColor="#55499b"
+                                as="u"
+                              >
+                                Read more
+                              </Text>
+                            </Box>
+                            <AccordionIcon />
+                          </AccordionButton>
+                        </h2>
+                        <AccordionPanel pb={4}>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　古くは、URC周辺のフォーク・シーンが築いた、日本民謡や日本人が好む泣きの要素を含んだ普遍的な郷士としての東京。はっぴぃえんどが示した、ポップ・ミュージックにおけるオノマトペを含んだ日本語の響さ。その後の彼らが標榜した、ウォール・オブ・サウンドのオリエンタルな解釈と、電子音楽で表現した未来都市としての東京。渋谷
+                            系のような局地的な土地性を持った音楽や、フリッパーズ・ギターの2人が解散後に発信した対称的な音楽。ceroに代表される、10年代以降のエクスペリメンタルな不定形のバンド/コレクティブたちとの並走。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　それらに加え、彼らにはボカロカルチャーや10年代以降のネット音楽が血肉として息づいている。ネイティブなネット世代による、東京インディの現在地として、一つの最適解と言えるだろう。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　そんな「uturu」には、相対性理論の作品を手がけ、『ほとんど、空』
+                            (2020)
+                            からバンドのサウンドを担う、米津裕二郎をレコーディング／ミックスエンジニアとして招聘しており、彼らを導く。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　また、今作はバンドで初となる Dolby Atmos、360
+                            Reality Audio
+                            に対応した楽曲であり、聴きどころの一つであるし、イマーシブ・オーディオを得意とする米津氏の起用は最適だ。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　ただ、前述のドメスティックな音楽は基礎的なもの。今作のリファレンスをコンパイルしたプレイリストが公開されているが、影響元は多岐にわたり、それらが組み合わされば特異なものになるのは必然だ。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　生まれた時からヒップホップに密接した世代特有のタイム感を持つ、Kassa
+                            Overall や Theo Croker
+                            のようなジャズマン。ポップミュージックの脱構築を試みる、Slauson
+                            Malone 1や Tara Clerkin Trio。偶
+                            発的な音をコントロールしてしまう、sora や Nuno
+                            Canavarroといった グリッチサウンドの使い手たち。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　250
+                            や公衆道徳（空中泥棒）らの極東音楽の矜持に触発され、bo
+                            en や Destroy Lonely の「Tokyo Mission
+                            Impossible」からカリカチュアされた外側からの東京観を受け取り、水いらずの国内でとどまることのないだろうポテンシャルを窺わせた。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　特に印象的だったのは、ボイス・サンプルの抜き差しをスティーブ
+                            ライヒの「Diffarent
+                            Trains」から引用し、それを日本語の擬音語に置き換えるというアイデアだ。最高に面白い。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　また、歌詞にも他の音楽からの影響を感じた。この曲に登場する「あなた」との関係性は、歌い手ごとに異なる印象を受け、一曲のうちに
+                            恋愛群像劇が展開されているかのよう。その「あなた」については、彼らが影響を受けた女性ボーカリストが投影されているようにも思える。戸川純や、人カボーカロイドが施された秋本会緒美の「Tennessee
+                            Waltz」、ASA-CHANG&巡礼の「花」で聴ける無機質な女性の語り
+                            など、エキセントリックかつ実像のない人物像を思わせる。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　ゲストボーカルとして米山ミサ（浮）を招き、まだ見ぬ架空の「あなた」や、誰かにとっての「あなた」を演出したのは素晴らしい手腕であるし、米山の演技力も筆舌に値する。下田開登
+                            (PICNIC
+                            YOU)による、飾らないフロウとリリックが浮き上がらせるコントラストもこの曲が持つ魅力だと言える。
+                            「uturu」は、「bakeru」(2022)
+                            からの1年ぶりの新曲だが、彼らの現在のモードは明らかに「su-mu」(2021)からの連続性にあるもので、表面的には牧歌的なムードだが、時折悍ましさを明かせるサウンドデザインや曲構成、歌詞に彼らのシグネチャーを見い出した。また、タイトルをアルファベット表記にすることで、聴き手に意味を委ねる同音異義語での言葉遊びも、彼らのシマーなイメージやムードを引き出している。
+                            日々の移ろいをフィルムに写し、グレインやブラーによって非現実的なものにすることもあれば、より克明な現実を映すこともある。今の水いらずはそんな表現をしてしまう。
+                          </Text>
+                        </AccordionPanel>
+                      </AccordionItem>
+                    </Accordion>
                   </Box>
                 </GridItem>
 
@@ -1077,17 +1147,81 @@ export default function UturuTemp() {
                       　東京を拠点とする4人組バンド、水いらずが届けてくれた新曲
                       「uturu」は、構想から約1年をかけた力作で、東京のインディ・ミュージックの分厚い歴史と未来を感じ取ることができる。
                     </Text>
-                    <Link href="/ReviewHiwatt">
-                      <Text
-                        fontSize="18px"
-                        fontWeight="bold"
-                        fontFamily="japanese2"
-                        textColor="#55499b"
-                        as="u"
-                      >
-                        Read more
-                      </Text>
-                    </Link>
+                    <Accordion allowToggle>
+                      <AccordionItem>
+                        <h2>
+                          <AccordionButton>
+                            <Box as="span" flex="1" textAlign="left">
+                              <Text
+                                fontSize="16px"
+                                fontWeight="bold"
+                                fontFamily="japanese2"
+                                textColor="#55499b"
+                                as="u"
+                              >
+                                Read more
+                              </Text>
+                            </Box>
+                            <AccordionIcon />
+                          </AccordionButton>
+                        </h2>
+                        <AccordionPanel pb={4}>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　古くは、URC周辺のフォーク・シーンが築いた、日本民謡や日本人が好む泣きの要素を含んだ普遍的な郷士としての東京。はっぴぃえんどが示した、ポップ・ミュージックにおけるオノマトペを含んだ日本語の響さ。その後の彼らが標榜した、ウォール・オブ・サウンドのオリエンタルな解釈と、電子音楽で表現した未来都市としての東京。渋谷
+                            系のような局地的な土地性を持った音楽や、フリッパーズ・ギターの2人が解散後に発信した対称的な音楽。ceroに代表される、10年代以降のエクスペリメンタルな不定形のバンド/コレクティブたちとの並走。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　それらに加え、彼らにはボカロカルチャーや10年代以降のネット音楽が血肉として息づいている。ネイティブなネット世代による、東京インディの現在地として、一つの最適解と言えるだろう。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　そんな「uturu」には、相対性理論の作品を手がけ、『ほとんど、空』
+                            (2020)
+                            からバンドのサウンドを担う、米津裕二郎をレコーディング／ミックスエンジニアとして招聘しており、彼らを導く。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　また、今作はバンドで初となる Dolby Atmos、360
+                            Reality Audio
+                            に対応した楽曲であり、聴きどころの一つであるし、イマーシブ・オーディオを得意とする米津氏の起用は最適だ。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　ただ、前述のドメスティックな音楽は基礎的なもの。今作のリファレンスをコンパイルしたプレイリストが公開されているが、影響元は多岐にわたり、それらが組み合わされば特異なものになるのは必然だ。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　生まれた時からヒップホップに密接した世代特有のタイム感を持つ、Kassa
+                            Overall や Theo Croker
+                            のようなジャズマン。ポップミュージックの脱構築を試みる、Slauson
+                            Malone 1や Tara Clerkin Trio。偶
+                            発的な音をコントロールしてしまう、sora や Nuno
+                            Canavarroといった グリッチサウンドの使い手たち。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　250
+                            や公衆道徳（空中泥棒）らの極東音楽の矜持に触発され、bo
+                            en や Destroy Lonely の「Tokyo Mission
+                            Impossible」からカリカチュアされた外側からの東京観を受け取り、水いらずの国内でとどまることのないだろうポテンシャルを窺わせた。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　特に印象的だったのは、ボイス・サンプルの抜き差しをスティーブ
+                            ライヒの「Diffarent
+                            Trains」から引用し、それを日本語の擬音語に置き換えるというアイデアだ。最高に面白い。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　また、歌詞にも他の音楽からの影響を感じた。この曲に登場する「あなた」との関係性は、歌い手ごとに異なる印象を受け、一曲のうちに
+                            恋愛群像劇が展開されているかのよう。その「あなた」については、彼らが影響を受けた女性ボーカリストが投影されているようにも思える。戸川純や、人カボーカロイドが施された秋本会緒美の「Tennessee
+                            Waltz」、ASA-CHANG&巡礼の「花」で聴ける無機質な女性の語り
+                            など、エキセントリックかつ実像のない人物像を思わせる。
+                          </Text>
+                          <Text fontSize="14px" fontFamily="japanese2" mt={1}>
+                            　ゲストボーカルとして米山ミサ（浮）を招き、まだ見ぬ架空の「あなた」や、誰かにとっての「あなた」を演出したのは素晴らしい手腕であるし、米山の演技力も筆舌に値する。下田開登
+                            (PICNIC
+                            YOU)による、飾らないフロウとリリックが浮き上がらせるコントラストもこの曲が持つ魅力だと言える。
+                            「uturu」は、「bakeru」(2022)
+                            からの1年ぶりの新曲だが、彼らの現在のモードは明らかに「su-mu」(2021)からの連続性にあるもので、表面的には牧歌的なムードだが、時折悍ましさを明かせるサウンドデザインや曲構成、歌詞に彼らのシグネチャーを見い出した。また、タイトルをアルファベット表記にすることで、聴き手に意味を委ねる同音異義語での言葉遊びも、彼らのシマーなイメージやムードを引き出している。
+                            日々の移ろいをフィルムに写し、グレインやブラーによって非現実的なものにすることもあれば、より克明な現実を映すこともある。今の水いらずはそんな表現をしてしまう。
+                          </Text>
+                        </AccordionPanel>
+                      </AccordionItem>
+                    </Accordion>
                   </Box>
                 </GridItem>
 
